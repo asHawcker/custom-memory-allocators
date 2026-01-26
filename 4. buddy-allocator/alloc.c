d9f8b742377bfcada9ca2290670ced459317361d
asHawcker <anuragsharma.takethat@gmail.com>
2026-01-26 23:46:53 +0530

    feat: buddy init function completed

--- 4. buddy-allocator/alloc.c
+++ 4. buddy-allocator/alloc.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <unistd.h>
 
 #define MAX_ORDER 8
 #define PAGE_SIZE 4096
@@ -17,3 +18,26 @@ typedef struct block_t
 static block_t *free_list[MAX_ORDER + 1];
 
 #define GET_BLOCK(ptr) ((block_t *)ptr)
+
+void buddy_init()
+{
+    heap_start = (uint8_t *)malloc(RAM_SIZE);
+    if (heap_start == NULL)
+    {
+        perror("Failed to allocate RAM");
+        exit(1);
+    }
+
+    for (int i = 0; i < MAX_ORDER + 1; i++)
+    {
+        free_list[i] = NULL;
+    }
+
+    block_t *root_block = (block_t *)heap_start;
+    root_block->order = MAX_ORDER;
+    root_block->is_free = 1;
+    root_block->next = NULL;
+    root_block->prev = NULL;
+
+    free_list[MAX_ORDER] = root_block;
+}
